bbafc94ee99cbee4c385d12cebf7c55e77d91bc0
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2024-05-17 16:20:26 +0530

    done

--- src/PrintPage.js
+++ src/PrintPage.js
@@ -2,12 +2,20 @@ import React from 'react'
 import "./index.css"
 
 function Slip({value}) {
+    
     return (
         <div className='slip'>
         {
             Object.keys(value).map((key)=>{
                 return <div className='roww'>
-                    {key.toUpperCase()} : {value[key].toUpperCase()}
+                    <div className='' style={{
+                        width: '29%',
+                        paddingLeft: '10px',
+                        // padding-left: '10px'
+                    }}>
+                    {key.toUpperCase()} :
+                    </div>                    
+                       {value[key].toUpperCase()}    
                 </div>
             })
         }
@@ -19,13 +27,18 @@ function PrintPage({value}) {
 //   console.log(value);
     let arr = [1,2,3,4,5,6,7,8,9,10];
     return (
+        <>
     <div className='flx'>
         {
-        arr.map((elem)=>{
-            return <Slip value={value} key={elem}/>
-        })
-    }
+            arr.map((elem)=>{
+                return <Slip value={value} key={elem}/>
+            })
+        }
+
+
     </div>
+
+        </>
   )
 }
 
